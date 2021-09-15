6ea67311ffc9f80fd2817ffbd0ed07bcc0d38776
Keebu <olkkuh@hotmail.com>
2021-09-15 16:08:39 +0300

    added thymeleaf template for /index

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -2,16 +2,13 @@ package com.example.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody // pois myöhemmin, että thymeleaf toimii
-
 public class BookController {
 
 	@GetMapping("/index")
 	public String index() {
-		return "index page";
+		return "index";
 	}
 
 }
