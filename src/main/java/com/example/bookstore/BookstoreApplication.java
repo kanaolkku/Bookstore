b72bda41e5894708f969fd1f4311c097c955dd49
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2021-10-28 12:06:09 +0300

    added tests

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -30,18 +30,19 @@ public class BookstoreApplication {
 			// categoryrepository.deleteAll();
 
 			log.info("adding categories");
+
 			// add categories
 			// categoryrepository.save(new Category("Cooking"));
 			// categoryrepository.save(new Category("Fantasy"));
 
 			log.info("adding books");
 			// add books
+
 			/*
 			 * bookrepository.save(new Book("kanaruokien kokkikirja", "iso kana", 2025,
 			 * "sad1238a", 13.99, categoryrepository.findByName("Cooking").get(0)));
 			 * bookrepository.save(new Book("Aakkoset aikuisille", "Varjo G", 2010,
-			 * "sasd1235a", 19.99, categoryrepository.findByName("Fantasy").get(0)));
-			 * 
+			 * "sasd1235a", 19.99, categoryrepository.findByName("Fantasy").get(0))); /*
 			 * User user1 = new User("user",
 			 * "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER"); User
 			 * user2 = new User("admin",
--- src/test/java/com/example/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/example/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.bookstore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.bookstore.web.BookController;
+import com.example.bookstore.web.BookControllerRest;
+import com.example.bookstore.web.CategoryController;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookstoreApplicationTests {
+	@Autowired
+	private BookController bookcontroller;
+	@Autowired
+	private CategoryController catcontroller;
+
+	@Autowired
+	private BookControllerRest bookcontrollerrest;
 
 	@Test
 	void contextLoads() {
+		assertThat(bookcontroller).isNotNull();
+		assertThat(catcontroller).isNotNull();
+		assertThat(bookcontrollerrest).isNotNull();
 	}
 
 }
