21f1436965e596c737058e06b57f14608d2c5dd9
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2021-10-28 13:23:35 +0300

    fix

--- src/main/java/com/example/bookstore/WebSecurityConfig.java
+++ src/main/java/com/example/bookstore/WebSecurityConfig.java
@@ -17,10 +17,9 @@ import com.example.bookstore.web.UserDetailServiceImpl;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**").permitAll().and().authorizeRequests()
-				.antMatchers("/signup", "/saveuser").permitAll().and().authorizeRequests().anyRequest().authenticated()
-				.and().formLogin().loginPage("/login").defaultSuccessUrl("/booklist").permitAll().and().logout()
-				.permitAll();
+		http.authorizeRequests().antMatchers("/css/**").permitAll().and().authorizeRequests().and().authorizeRequests()
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").defaultSuccessUrl("/booklist")
+				.permitAll().and().logout().permitAll();
 	}
 
 	@Autowired
